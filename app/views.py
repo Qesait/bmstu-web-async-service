d2e79224322ffcadfdec09ce6041b19ceb72fd48
Qesait <kuzva2003@gmail.com>
2023-12-10 00:57:16 +0300

    example

---
+++ app/views.py
@@ -0,0 +1,41 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+import time
+import random
+import requests
+
+from concurrent import futures
+
+CALLBACK_URL = "http://localhost:8080/api/tranpostations/"
+
+executor = futures.ThreadPoolExecutor(max_workers=1)
+
+def get_random_status(pk):
+    time.sleep(5)
+    return {
+      "id": pk,
+      "status": bool(random.getrandbits(1)),
+    }
+
+def status_callback(task):
+    try:
+      result = task.result()
+      print(result)
+    except futures._base.CancelledError:
+      return
+    
+    nurl = str(CALLBACK_URL+str(result["id"])+'/put/')
+    answer = {"is_growing": result["status"]}
+    requests.put(nurl, data=answer, timeout=3)
+
+@api_view(['POST'])
+def set_status(request):
+    if "pk" in request.data.keys():   
+        id = request.data["pk"]        
+
+        task = executor.submit(get_random_status, id)
+        task.add_done_callback(status_callback)        
+        return Response(status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
