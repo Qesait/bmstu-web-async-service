d52a1d1cd11c829cb53c885dfac8e50a675d106a
Qesait <kuzva2003@gmail.com>
2023-12-10 22:21:52 +0300

    my variant

--- app/views.py
+++ app/views.py
@@ -8,34 +8,37 @@ import requests
 
 from concurrent import futures
 
-CALLBACK_URL = "http://localhost:8080/api/tranpostations/"
+CALLBACK_URL = "http://localhost:8080/api/transportations/"
 
 executor = futures.ThreadPoolExecutor(max_workers=1)
+TOKEN = 'secret_token'
 
-def get_random_status(pk):
+
+def get_random_status(transportation_id):
     time.sleep(5)
     return {
-      "id": pk,
-      "status": bool(random.getrandbits(1)),
+        "transportation_id": transportation_id,
+        "status": bool(random.randint(0, 3)),
     }
+
 
 def status_callback(task):
     try:
-      result = task.result()
-      print(result)
+        result = task.result()
+        print(result)
     except futures._base.CancelledError:
-      return
-    
-    nurl = str(CALLBACK_URL+str(result["id"])+'/put/')
-    answer = {"is_growing": result["status"]}
-    requests.put(nurl, data=answer, timeout=3)
+        return
+
+    url = str(CALLBACK_URL+str(result["transportation_id"])+'/delivery/')
+    requests.put(url, data={"delivery_status": result['status'], "token": TOKEN}, timeout=3)
+
 
 @api_view(['POST'])
 def set_status(request):
-    if "pk" in request.data.keys():   
-        id = request.data["pk"]        
+    if "transportation_id" in request.data.keys():
+        transportation_id = request.data["transportation_id"]
 
-        task = executor.submit(get_random_status, id)
-        task.add_done_callback(status_callback)        
+        task = executor.submit(get_random_status, transportation_id)
+        task.add_done_callback(status_callback)
         return Response(status=status.HTTP_200_OK)
     return Response(status=status.HTTP_400_BAD_REQUEST)
